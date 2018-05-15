bb4bf35086df2de5016b6ae53ce38d87e7ee3c30
Sean Gillespie <sean@swgillespie.me>
2018-05-15 00:08:56 -0700

    Fix bench

--- apollo/benches/check_benches.rs
+++ apollo/benches/check_benches.rs
@@ -10,13 +10,13 @@
 extern crate apollo;
 extern crate test;
 
-use apollo::{Position, Color};
+use apollo::{Engine, Position, Color};
 use test::Bencher;
 
 #[bench]
 fn starting_position_check_bench(b: &mut Bencher) {
-    apollo::initialize();
-    let pos = Position::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
+    let engine = Engine::new();
+    let pos = engine.new_position_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
     b.iter(|| pos.is_check(Color::White));
 }
 
@@ -24,8 +24,8 @@ fn starting_position_check_bench(b: &mut Bencher) {
 fn fools_mate_checkmate_bench(b: &mut Bencher) {
     // this benchmark is a reminder that testing for checkmate is potentially
     // expensive.
-    apollo::initialize();
-    let pos = Position::from_fen("rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR w KQkq - 0 1").unwrap();
+    let engine = Engine::new();
+    let pos = engine.new_position_from_fen("rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR w KQkq - 0 1").unwrap();
     b.iter(|| pos.is_checkmate());
 }
 
@@ -33,7 +33,7 @@ fn fools_mate_checkmate_bench(b: &mut Bencher) {
 fn midgame_negative_checkmate_bench(b: &mut Bencher) {
     // this benchmark is a reminder of the cost for testing for checkmate
     // when the player is in check but not checkmated.
-    apollo::initialize();
-    let pos = Position::from_fen("r3k1nr/1b1p2pp/p2b2Q1/n3p1B1/2B5/2N2N2/PP3PPP/R3R1K1 b kq - 0 1").unwrap();
+    let engine = Engine::new();
+    let pos = engine.new_position_from_fen("r3k1nr/1b1p2pp/p2b2Q1/n3p1B1/2B5/2N2N2/PP3PPP/R3R1K1 b kq - 0 1").unwrap();
     b.iter(|| pos.is_checkmate());
 }
